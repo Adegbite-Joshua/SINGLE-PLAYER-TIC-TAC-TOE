7fb734113546c17827b7d2fd75f943ae0a689075
ADEGBITE JOSHUA ADEOYE <adegbitejoshua07@gmail.com>
2023-05-02 17:26:52 +0100

    Working on the player name display

--- src/Components/Game.jsx
+++ src/Components/Game.jsx
@@ -1,22 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Game = () => {
-        let players = [
-            'Ade',
-            'Kola'
-        ]
-        // const showPlayers =()=>{
-        //     fPSN.innerText = 1
-        //     sPSN.innerText = 2
-        //     firstSign.innerText = "X"
-        //     secondSign.innerText = "O"
-        //     firstScore.innerText = 0
-        //     secondScore.innerText = 0 
-        //     firstPlayer.innerText = players[0]
-        //     secondPlayer.innerText = players[1]
-        //     let nextPlayer = "X"
-        //     nextPlayerToPlay.innerText = `It is ${players[0]}'s turn to play, playing as "X"`
-        // }
+    const [allTictactoePlayer, setallTictactoePlayer] = useState(JSON.parse(localStorage.getItem('tictactoePlayerDetails')))
+    const [currentPlayer, setcurrentPlayer] = useState(Number(localStorage.getItem('currentTictactoePlayer')))
+        useEffect(() => {
+            let newallTictactoePlayer = JSON.parse(localStorage.getItem('tictactoePlayerDetails'))
+            setallTictactoePlayer(newallTictactoePlayer)
+            showPlayers()
+            console.log(allTictactoePlayer);
+
+        }, [])
+        // let details = localStorage.getItem('tictactoePlayerDetails')
+        let players = []
+        let nextPlayer = 'X'
+        const showPlayers =()=>{
+            players = [ allTictactoePlayer[currentPlayer].name , 'computer']
+            fPSN.innerText = 1
+            sPSN.innerText = 2
+            firstSign.innerText = "X"
+            secondSign.innerText = "O"
+            firstScore.innerText = 0
+            secondScore.innerText = 0 
+            firstPlayer.innerText = players[0]
+            secondPlayer.innerText = players[1]
+            let nextPlayer = "X"
+            nextPlayerToPlay.innerText = `It is ${players[0]}'s turn to play, playing as "X"`
+        }
         const playGame =(e)=>{
             if (boxa1.innerText=="X" && boxb2.innerText=="X" && boxc3.innerText=="X" || boxa2.innerText=="X" && boxb2.innerText=="X" && boxc2.innerText=="X" || boxa3.innerText=="X" && boxb2.innerText=="X" && boxc1.innerText=="X" || boxb1.innerText=="X" && boxb2.innerText=="X" && boxb3.innerText=="X" || boxa1.innerText=="X" && boxa2.innerText=="X" && boxa3.innerText=="X" || boxc1.innerText=="X" && boxc2.innerText=="X" && boxc3.innerText=="X" || boxa1.innerText=="X" && boxb1.innerText=="X" && boxc1.innerText=="X" || boxa3.innerText=="X" && boxb3.innerText=="X" && boxc3.innerText=="X" || boxa1.innerText=="O" && boxb2.innerText=="O" && boxc3.innerText=="O" || boxa2.innerText=="O" && boxb2.innerText=="O" && boxc2.innerText=="O" || boxa3.innerText=="O" && boxb2.innerText=="O" && boxc1.innerText=="O" || boxb1.innerText=="O" && boxb2.innerText=="O" && boxb3.innerText=="O" || boxa1.innerText=="O" && boxa2.innerText=="O" && boxa3.innerText=="O" || boxc1.innerText=="O" && boxc2.innerText=="O" && boxc3.innerText=="O" || boxa1.innerText=="O" && boxb1.innerText=="O" && boxc1.innerText=="O" || boxa3.innerText=="O" && boxb3.innerText=="O" && boxc3.innerText=="O") {
                 alert("Click on continue")
@@ -42,8 +51,19 @@ const Game = () => {
                     winner.innerText = `Draw 😰`
                 }
             }
-                
+            setTimeout(computerPlaying, 900) 
         }
+
+        const computerPlaying =()=>{
+            let randonNumber = Math.round(Math.random()*9)
+            console.log(randonNumber)
+        }
+        // for (let i = 0; i < 100; i++) {
+        //     let randonNumber = Math.round(Math.random()*9)
+        //     if (randonNumber >= 9) {
+        //         console.log(randonNumber)
+        //     }
+        // }
         const continueGame =()=>{
             boxa1.innerText = ""
             boxa2.innerText = ""
@@ -97,30 +117,61 @@ const Game = () => {
         }
   return (
     <>
-        <div className='w-50 vh-100 d-block mx-auto border border-2'>
+        <div className='w-100 vh-100 row row-cols-1 row-cols-md-2 overflow-hidden position-relative border border-2'>
             <div className="smallDeviceHeight">
                 <div style={{height: '300px', width: '300px', display: 'flex', flexWrap: 'wrap'}} className="mx-auto">
-                    <div id="boxa1" className="border border-2 checkBox m-auto rounded-2 border-danger danger d-flex justify-content-center align-items-center fw-bold" onClick={()=>playGame(event)}></div>
-                    <div id="boxa2" className="border border-2 checkBox m-auto rounded-2 border-success success d-flex justify-content-center align-items-center fw-bold" onClick={()=>playGame(event)}></div>
-                    <div id="boxa3" className="border border-2 checkBox m-auto rounded-2 border-success success d-flex justify-content-center align-items-center fw-bold" onClick={()=>playGame(event)}></div>
-                    <div id="boxb1" className="border border-2 checkBox m-auto rounded-2 border-warning warning d-flex justify-content-center align-items-center fw-bold" onClick={()=>playGame(event)}></div>
-                    <div id="boxb2" className="border border-2 checkBox m-auto rounded-2 border-danger danger d-flex justify-content-center align-items-center fw-bold" onClick={()=>playGame(event)}></div>
-                    <div id="boxb3" className="border border-2 checkBox m-auto rounded-2 border-success success d-flex justify-content-center align-items-center fw-bold" onClick={()=>playGame(event)}></div>
-                    <div id="boxc1" className="border border-2 checkBox m-auto rounded-2 border-warning warning d-flex justify-content-center align-items-center fw-bold" onClick={()=>playGame(event)}></div>
-                    <div id="boxc2" className="border border-2 checkBox m-auto rounded-2 border-warning warning d-flex justify-content-center align-items-center fw-bold" onClick={()=>playGame(event)}></div>
-                    <div id="boxc3" className="border border-2 checkBox m-auto rounded-2 border-danger danger d-flex justify-content-center align-items-center fw-bold" onClick={()=>playGame(event)}></div>
+                    <div id="boxa1" className="border border-2 checkBox m-auto rounded-2 border-danger danger d-flex justify-content-center align-items-center fw-bold" onClick={(e)=>playGame(e)}></div>
+                    <div id="boxa2" className="border border-2 checkBox m-auto rounded-2 border-success success d-flex justify-content-center align-items-center fw-bold" onClick={(e)=>playGame(e)}></div>
+                    <div id="boxa3" className="border border-2 checkBox m-auto rounded-2 border-success success d-flex justify-content-center align-items-center fw-bold" onClick={(e)=>playGame(e)}></div>
+                    <div id="boxb1" className="border border-2 checkBox m-auto rounded-2 border-warning warning d-flex justify-content-center align-items-center fw-bold" onClick={(e)=>playGame(e)}></div>
+                    <div id="boxb2" className="border border-2 checkBox m-auto rounded-2 border-danger danger d-flex justify-content-center align-items-center fw-bold" onClick={(e)=>playGame(e)}></div>
+                    <div id="boxb3" className="border border-2 checkBox m-auto rounded-2 border-success success d-flex justify-content-center align-items-center fw-bold" onClick={(e)=>playGame(e)}></div>
+                    <div id="boxc1" className="border border-2 checkBox m-auto rounded-2 border-warning warning d-flex justify-content-center align-items-center fw-bold" onClick={(e)=>playGame(e)}></div>
+                    <div id="boxc2" className="border border-2 checkBox m-auto rounded-2 border-warning warning d-flex justify-content-center align-items-center fw-bold" onClick={(e)=>playGame(e)}></div>
+                    <div id="boxc3" className="border border-2 checkBox m-auto rounded-2 border-danger danger d-flex justify-content-center align-items-center fw-bold" onClick={(e)=>playGame(e)}></div>
                 </div>
                 <p id="nextPlayerToPlay" className="text-success text-center"></p>
                 <div className="mx-auto w-50 d-block btn-group">
-                    <button className="btn btn-success ms-auto my-2" onClick={()=>playMusic()}>Play Sound</button>
-                    <button className="btn btn-success me-auto my-2" onClick={()=>pauseMusic()}>Pause Sound</button>
+                    <button className="btn btn-success ms-auto my-2" onClick={playMusic}>Play Sound</button>
+                    <button className="btn btn-success me-auto my-2" onClick={pauseMusic}>Pause Sound</button>
                 </div>
-                <button className="btn btn-success d-block w-50 mx-auto my-2" onClick={()=>continueGame()}>Continue</button>
-                <button className="btn btn-success d-block w-50 mx-auto my-2" onClick={()=>restartGame()}>Restart</button>
-                <button className="btn btn-success d-block w-50 mx-auto my-2" onClick={()=>resetGame()}>Reset</button>
+                <button className="btn btn-success d-block w-50 mx-auto my-2" onClick={continueGame}>Continue</button>
+                <button className="btn btn-success d-block w-50 mx-auto my-2" onClick={restartGame}>Restart</button>
+                <button className="btn btn-success d-block w-50 mx-auto my-2" onClick={resetGame}>Reset</button>
+            </div>
+            <div className="smallDeviceHeight2">
+                {/* <marquee behavior="" direction=""><h1 className="mt-5 text-success">TIC TAC TOE GAME</h1></marquee> */}
+                <table style={{width: '90%'}} className="text-center mt-5 mx-auto">
+                    <thead>
+                        <tr>
+                            <td className="border border-2 border-success text-success">S/N</td>
+                            <td className="border border-2 border-success text-success">PLAYER'S NAME</td>
+                            <td className="border border-2 border-success text-success">SIGN</td>
+                            <td className="border border-2 border-success text-success">SCORE</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td className="border border-2 border-success text-success" id="fPSN"></td>
+                            <td className="border border-2 border-success text-success" id="firstPlayer"></td>
+                            <td className="border border-2 border-success text-success" id="firstSign"></td>
+                            <td className="border border-2 border-success text-success" id="firstScore"></td>
+                        </tr>
+                        <tr>
+                            <td className="border border-2 border-success text-success" id="sPSN"></td>
+                            <td className="border border-2 border-success text-success" id="secondPlayer"></td>
+                            <td className="border border-2 border-success text-success" id="secondSign"></td>
+                            <td className="border border-2 border-success text-success" id="secondScore"></td>
+                        </tr>
+                    </tbody>
+                </table>
+                <marquee behavior="scroll" direction="left"><h4 id="winner"></h4></marquee>
+            </div>
+            <div className="continueDiv">
+                <p>Click on continue game to continue</p>
             </div>
         </div>
-        {/* {showPlayers()} */}
+        
     </>
   )
 }
--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -1,33 +1,62 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 
 const LandingPage = () => {
   const navigate = useNavigate();
+  const [allTictactoePlayer, setallTictactoePlayer] = useState([])
+  const [foundUser, setfoundUser] = useState(false)
   useEffect(() => {
     document.querySelector('input').focus()
+    if (localStorage.allTictactoePlayer) {
+      let newallTictactoePlayer = JSON.parse(localStorage.getItem('tictactoePlayerDetails'))
+      setallTictactoePlayer(newallTictactoePlayer)
+    }
+    console.log(allTictactoePlayer)
   }, [])
   
   const startGame =()=>{
     let playerName = document.querySelector('input').value.trim()
     if (playerName!="") {
-      let newPlayerDetails = {
-        name: playerName,
-        score: {
-          player: 0,
-          computer: 0
-        },
-        tableDisplay: {
-          
+      allTictactoePlayer.map((user, index)=>{
+        if (user.name==playerName) {
+          localStorage.setItem('currentTictactoePlayer', index);
+          let newfoundUser = true;
+          setfoundUser(newfoundUser)
+          // break;
         }
-
+      })
+      if (foundUser) {
+        // navigate('/dashboard')
+      } else{
+        let newPlayerDetails = {
+          name: playerName,
+          score: [
+            0,
+            0
+          ],
+          tableDisplay: [
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+          ]
+        }
+        // allTictactoePlayer.push(newPlayerDetails)
+        setallTictactoePlayer([...allTictactoePlayer, newPlayerDetails])
+        localStorage.setItem('tictactoePlayerDetails', JSON.stringify(allTictactoePlayer))
+        localStorage.setItem('currentTictactoePlayer', allTictactoePlayer.length);
+        // navigate('/dashboard')
       }
-      localStorage.setItem('tictactoePlayerName', playerName)
-      navigate('/dashboard')
     } else{
-        document.querySelector('p').style.display = "block";
-        setTimeout(()=>document.querySelector('p').style.display = "none", 3000);
-        document.querySelector('input').focus()
+      document.querySelector('p').style.display = "block";
+      setTimeout(()=>document.querySelector('p').style.display = "none", 3000);
+      document.querySelector('input').focus()
     }
   }
 
@@ -36,6 +65,7 @@ const LandingPage = () => {
         <div className="w-100 vh-100 bg-info d-flex align-items-center justify-content-center">
           {/* <div style={{aspectRatio: '1'}} className='w-50 d-block mx-auto border border-2'> */}
               <div className='nameDiv w-50 h-50 d-block mx-auto border border-2 d-flex align-items-center justify-content-center flex-column px-5'>
+                  <p className='text-light text-center'>Enter your name to crate an account or continue your game</p>
                   <input type="text" placeholder='Enter your name' className='form-control my-2 text-center'/>
                   <p style={{display: 'none'}} className='text-light'>Please input a name to continue</p>
                   <button onClick={startGame} className="btn btn-warning w-100">Start</button>
